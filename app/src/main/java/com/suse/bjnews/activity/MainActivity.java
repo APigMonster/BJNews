fae1763dfc2cc651e92d417fecf9b7ce80ee6669
xmc <xmc@163.com>
2019-01-28 10:39:21 +0800

    利用SlidingMenu设置滑动菜单  导入SlidingMenu包   修改包下的错误，所需要的版本变化会导致出现错误  添加SlidingMenu依赖到项目，并设置基本参数

--- app/src/main/java/com/suse/bjnews/activity/MainActivity.java
+++ app/src/main/java/com/suse/bjnews/activity/MainActivity.java
@@ -3,13 +3,26 @@ package com.suse.bjnews.activity;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
+import com.jeremyfeinstein.slidingmenu.lib.app.SlidingFragmentActivity;
 import com.suse.bjnews.R;
+import com.suse.bjnews.utils.DensityUtil;
 
-public class MainActivity extends Activity {
+public class MainActivity extends SlidingFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //设置主页面
         setContentView(R.layout.activity_main);
+        //设置左边菜单
+        setBehindContentView(R.layout.activity_leftmenu);
+        //设置显示模式
+        SlidingMenu slidingMenu = getSlidingMenu();
+        slidingMenu.setMode(SlidingMenu.LEFT);
+        //设置滑动模式
+        slidingMenu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+        //设置主页面占用宽度
+        slidingMenu.setBehindOffset(DensityUtil.dip2px(MainActivity.this, 270));
     }
 }
